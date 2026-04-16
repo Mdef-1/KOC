d2f8091dd9d8df95771392cf92e38c154af08a08
murdo <murdowasfind@gmail.com>
2026-04-16 08:30:26 +0700

    fix: update categories to relevant apparel types for KOC

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,13 +16,13 @@ class CategorySeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $categories = [
-            ['name' => 'Elektronik', 'slug' => 'elektronik', 'description' => 'Kategori untuk produk-produk elektronik'],
-            ['name' => 'Fashion', 'slug' => 'fashion', 'description' => 'Kategori untuk produk-produk fashion'],
-            ['name' => 'Kecantikan', 'slug' => 'kecantikan', 'description' => 'Kategori untuk produk-produk kecantikan'],
-            ['name' => 'Rumah Tangga', 'slug' => 'rumah-tangga', 'description' => 'Kategori untuk peralatan rumah tangga'],
-            ['name' => 'Olahraga', 'slug' => 'olahraga', 'description' => 'Kategori untuk peralatan olahraga'],
-            ['name' => 'Mainan', 'slug' => 'mainan', 'description' => 'Kategori untuk mainan anak-anak'],
-            ['name' => 'Makanan & Minuman', 'slug' => 'makanan-minuman', 'description' => 'Kategori untuk produk makanan dan minuman'],
+            ['name' => 'Kaos Polos', 'slug' => 'kaos-polos', 'description' => 'Kaos polos tanpa sablon, ready stock'],
+            ['name' => 'Kaos Raglan', 'slug' => 'kaos-raglan', 'description' => 'Kaos raglan lengan panjang/pendek'],
+            ['name' => 'Polo Shirt', 'slug' => 'polo-shirt', 'description' => 'Kaos berkerah dengan kancing'],
+            ['name' => 'Hoodie & Sweater', 'slug' => 'hoodie-sweater', 'description' => 'Hoodie dan sweater polos'],
+            ['name' => 'Kaos V-Neck', 'slug' => 'kaos-v-neck', 'description' => 'Kaos kerah V'],
+            ['name' => 'Long Sleeve', 'slug' => 'long-sleeve', 'description' => 'Kaos lengan panjang'],
+            ['name' => 'Tank Top', 'slug' => 'tank-top', 'description' => 'Kaos tanpa lengan'],
         ];
 
         foreach ($categories as $category) {
